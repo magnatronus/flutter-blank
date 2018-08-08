4c98df51a8166614319d9a9c9b40937910841148
SpiralArm Consulting Ltd <projects@spiralarm.uk>
2018-08-08 08:16:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# blankapp
+
+A Barebones Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view the online
+[documentation](https://flutter.io/).
---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+///
+/// Simple Scaffold Widget
+///
+class MainApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Blank App")
+      ),
+    );
+  }
+}
